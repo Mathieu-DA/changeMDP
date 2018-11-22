2a66651203d1ddd0381c1f8960953e7d2411564b
Mathieu De Armey <mdearmey@gmail.com>
2018-11-22 18:58:09 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Breached from './breached.jsx';
 import './App.css';
+import { Button } from 'reactstrap'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      info: [],
+      clicked: false,
+    }
+  }
+
+
+  handleInput = (e) => {
+    this.setState({ email: e.target.value })
+  }
+
+  handleSubmit = (email) => {
+    this.setState({ info: [], clicked: true })
+    axios.get("https://haveibeenpwned.com/api/breachedaccount/" + email)
+      .then(res => {
+        const site = res.data
+        const allInfo = this.state.info
+        for (let i = 0; i < site.length; i++) {
+          axios.get("https://haveibeenpwned.com/api/v2/breach/" + site[i])
+            .then(res => {
+              const info = res.data
+              allInfo.push(info)
+              this.setState({ info: allInfo })
+            })
+        }
+      })
+  }
+
+
+
+  enterPassword = (e) => {
+    if (e.key === 'Enter') {
+      this.handleSubmit(this.state.email)
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <body>
+        <section className="input">
+        <h1>
+          Entre ton adresse mail 
+        </h1>
+          <input className="emailinput" value={this.state.email} onChange={this.handleInput} onKeyPress={this.enterPassword} placeholder="Email"></input>
+          <Button onClick={() => this.handleSubmit(this.state.email)}>Submit</Button>
+        </section>
+        {this.state.info.length >0  ? 
+        <div className="pwned"><h2>Saperlipopette!!</h2><h4>Tes infos ont été compromises sur {this.state.info.length} site{this.state.info.length > 1 ? "s" : ""} piraté{this.state.info.length > 1 ? "s" : ""}.<br></br>
+        Va changer ton mot de passe</h4></div>
+         : this.state.clicked ? <h2 className="pwned bien">Tout va bien</h2> : ""}
+        <div className="list">
+
+          {this.state.info.map(site => <ul><Breached site={site} /></ul>)}
+
+
+        </div>
+      </body >
     );
   }
 }
---
+++ src/breached.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { UncontrolledCollapse, Button, CardBody, Card, CardImg, CardTitle, } from 'reactstrap';
+import "./breached.css";
+
+
+class Breached extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logo: "https://haveibeenpwned.com/Content/Images/PwnedLogos/" + this.props.site.Name + "." + this.props.site.LogoType,
+      id: this.props.site.Name.replace(/[0-9]/g, '')
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Card className="cards">
+          <CardImg className="cardImage" top width="100%" src={this.state.logo} alt={this.props.site.Name} />
+          <CardBody>
+            <CardTitle><u>Site piraté</u>: <p>{this.props.site.Name}</p></CardTitle>
+            <p className="lead"><u>Données compromises</u>: {this.props.site.DataClasses.map(c => <span>{c}, </span>)}</p>
+            <p><u>Date de la violation</u>: {this.props.site.BreachDate}</p>
+            <div id={this.state.id} style={{ marginBottom: '1rem' }} className="description">+ d'informations</div>
+            <UncontrolledCollapse toggler={"#" + this.state.id}>
+              <Card>
+                <CardBody className="cardbody">
+                <div dangerouslySetInnerHTML={{__html: this.props.site.Description}} />
+                </CardBody>
+              </Card>
+            </UncontrolledCollapse>
+            {this.props.site.Domain === "" ? <div></div> : 
+            <a href={"https://" + this.props.site.Domain} target="_blank" rel="noopener noreferrer">
+            <Button className="buttons" color="primary" style={{ marginBottom: '1rem' }}>Change ton MdP</Button></a>}
+          </CardBody>
+        </ Card>
+      </div>
+
+    )
+  }
+}
+
+
+export default Breached;

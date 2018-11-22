863eb9a72c97e2a95264d0a1cb758b1338e540fb
Mathieu De Armey <mdearmey@gmail.com>
2018-11-22 19:03:18 +0100

    Updates

---
+++ precache-manifest.255df1bd0f3dda104d2585d1b8a45883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ffd5fe22d90bf04f90f",
+    "url": "/changeMDP/static/js/runtime~main.3ffd5fe2.js"
+  },
+  {
+    "revision": "be3553929907be8b15a5",
+    "url": "/changeMDP/static/js/main.be355392.chunk.js"
+  },
+  {
+    "revision": "a290e2bfc4ab94108050",
+    "url": "/changeMDP/static/js/1.a290e2bf.chunk.js"
+  },
+  {
+    "revision": "be3553929907be8b15a5",
+    "url": "/changeMDP/static/css/main.78259987.chunk.css"
+  },
+  {
+    "revision": "a290e2bfc4ab94108050",
+    "url": "/changeMDP/static/css/1.fb407a8a.chunk.css"
+  },
+  {
+    "revision": "50e17dcfc7369a6f303746c80132490b",
+    "url": "/changeMDP/index.html"
+  }
+];
